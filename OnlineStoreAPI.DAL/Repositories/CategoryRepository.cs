f17eee6ffdc08e2defd9dd266e13508164478d10
ZLUKADARK <ahmadcakaev@mail.ru>
2024-10-10 15:16:39 +0600

    Updated item categories

--- OnlineStoreAPI.DAL/Repositories/CategoryRepository.cs
+++ OnlineStoreAPI.DAL/Repositories/CategoryRepository.cs
@@ -38,17 +38,29 @@ namespace OnlineStoreAPI.DAL.Repositories
 
         public async Task<Category> DeleteAsync(int id)
         {
+            var transaction = await _db.Database.BeginTransactionAsync();
             try
             {
-                var data = await _db.Categories.FindAsync(id);
-                _db.Categories.Remove(data);
+                var requst = await _db.Categories
+                    .AsNoTracking()
+                    .Include(x => x.ItemProperty)
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                var result = _db.Categories
+                    .Remove(requst);
+                _db.ItemPropertis.RemoveRange(requst.ItemProperty);
+
                 await _db.SaveChangesAsync();
+                await transaction.CommitAsync();
+
                 await _cacheServices.OnDeleteAsync<Category>(id.ToString(), "categories", 1, x => x.Id == id);
-                return data;
+                await _cacheServices.DeleteAsync("items");
+                return result.Entity;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                _logger.LogError(ex, $"Error when try delete item with id: \"{id}\" in category repository");
+                _logger.LogCritical(ex, $"Error when delete ItemCategory {id}");
+                await transaction.RollbackAsync();
                 throw ex;
             }
         }
@@ -61,7 +73,9 @@ namespace OnlineStoreAPI.DAL.Repositories
                 category = await _cacheServices.OnGetAsync<Category>(id.ToString());
                 if(category == null)
                 {
-                    category = await _db.Categories.FindAsync(id);
+                    category = await _db.Categories.Include(x => x.ItemProperty)
+                        .AsNoTracking()
+                        .FirstAsync(x => x.Id == id);
                     await _cacheServices.AddAsync(id.ToString(), category, 1);
                 }
                 return category;
@@ -101,11 +115,97 @@ namespace OnlineStoreAPI.DAL.Repositories
                 entity.State = EntityState.Modified;
                 await _db.SaveChangesAsync();
                 await _cacheServices.OnUpdateAsync<Category>(data.Id.ToString(), "categories", entity.Entity, 1, x => x.Id == data.Id);
+                await _cacheServices.DeleteAsync(data.Id.ToString());
+                await _cacheServices.DeleteAsync("items");
                 return entity.Entity;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, $"Error when try update item with id: \"{data.Id}\" in category repository");
+                _logger.LogCritical(ex, $"Error when update Category {data.Id}");
+                throw ex;
+            }
+        }
+
+        public async Task<Category> AddPropertyAsync(Category data)
+        {
+            var transaction = await _db.Database.BeginTransactionAsync();
+            try
+            {
+                var result = await _db.Categories
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == data.Id);
+                result.ItemProperty.AddRange(data.ItemProperty);
+                _db.Categories.Update(result);
+                await _db.SaveChangesAsync();
+                await transaction.CommitAsync();
+                await _cacheServices.DeleteAsync(data.Id.ToString());
+                await _cacheServices.DeleteAsync("items");
+                await _cacheServices.DeleteAsync("categories");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, $"Error when add Category to item {data.Id}");
+                await transaction.RollbackAsync();
+                throw ex;
+            }
+        }
+
+        public async Task<Category> DeletePropertyAsync(Category data)
+        {
+            var transaction = await _db.Database.BeginTransactionAsync();
+            try
+            {
+                var result = await _db.Categories
+                .Include(x => x.ItemProperty)
+                .FirstOrDefaultAsync(x => x.Id == data.Id);
+
+                result.ItemProperty.RemoveAll(x => data.ItemProperty.Any(e => e.Id == x.Id));
+
+                _db.Entry(result).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                await transaction.CommitAsync();
+                await _cacheServices.OnUpdateAsync<Category>(data.Id.ToString(), "categories", result, 1, x => x.Id == data.Id);
+                await _cacheServices.DeleteAsync("items");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, $"Error when delete Category from item {data.Id}");
+                await transaction.RollbackAsync();
+                throw ex;
+            }
+        }
+
+        public async Task<Category> UpdatePropertyAsync(Category data)
+        {
+            var transaction = await _db.Database.BeginTransactionAsync();
+            try
+            {
+                var result = await _db.Categories
+                .Include(x => x.ItemProperty)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == data.Id);
+                //Нужно пофиксить
+                foreach (var property in data.ItemProperty)
+                {
+                    int index = result.ItemProperty.IndexOf(result.ItemProperty.FirstOrDefault(x => x.Id == property.Id));
+                    result.ItemProperty[index] = property;
+                    var itemPropertyEntity = _db.Entry<ItemProperty>(result.ItemProperty[index]);
+                    itemPropertyEntity.State = EntityState.Modified;
+                }
+
+                await _db.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                await _cacheServices.OnUpdateAsync<Category>(data.Id.ToString(), "categories", result, 1, x => x.Id == data.Id);
+                await _cacheServices.DeleteAsync("items");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, $"Error when delete Category to item {data.Id}");
+                await transaction.RollbackAsync();
                 throw ex;
             }
         }
