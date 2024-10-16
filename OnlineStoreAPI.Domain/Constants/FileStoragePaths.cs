1ba5a2d7d5f00988ba5f3c92848357c9b6524a6b
ZLUKADARK <ahmadcakaev@mail.ru>
2024-10-16 14:04:08 +0600

    change paths

--- OnlineStoreAPI.Domain/Constants/FileStoragePaths.cs
+++ OnlineStoreAPI.Domain/Constants/FileStoragePaths.cs
@@ -2,8 +2,8 @@
 {
     public static class FileStoragePaths
     {
-        public const string CategoryPath = "Categories/{0}";
-        public const string ItemPath = "Items/{0}";
-        public const string CompanyPath = "Companies/{0}";
+        public const string CategoryPath = "Categories/{0}/";
+        public const string ItemPath = "Items/{0}/";
+        public const string CompanyPath = "Companies/{0}/";
     }
 }
